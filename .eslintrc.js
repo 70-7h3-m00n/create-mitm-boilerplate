4136fa6369e4384fd580b2fbe24a5d2caaad6608
Anissem <dima.anissem@gmail.com>
2022-11-03 02:15:36 +0300

    feat(husky): add husky and git-staged
    add husky and lint-staged for pre-commit linting
    
    add .prettierignore and .eslintignore files for ignoring linting
    building artefacts, config and generated on build files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,37 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true
-    },
-    extends: [
-        'next',
-        'next/core-web-vitals',
-        'plugin:@typescript-eslint/recommended',
-        'airbnb',
-        'prettier'
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true
-        }
-    },
-    plugins: [
-        '@typescript-eslint'
-    ],
-    rules: {
-        'react/jsx-filename-extension': ['warn', { 'extensions': ['.tsx'] }],
-        'react/react-in-jsx-scope': 0,
-        'react/jsx-uses-react': 0,
-        'quotes': ['error', 'single']
-    },
-    'globals': {
-        JSX: 'readonly'
-    },
-    overrides: [
-        {
-            files: ['./app/**/layout.tsx'],
-            rules: {
-                '@next/next/no-head-element': 0
-            }
-        }
-    ]
-};
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-uses-react': 0,
+    quotes: ['error', 'single']
+  },
+  globals: {
+    JSX: 'readonly'
+  },
+  overrides: [
+    {
+      files: ['./app/**/layout.tsx'],
+      rules: {
+        '@next/next/no-head-element': 0
+      }
+    }
+  ]
+}
