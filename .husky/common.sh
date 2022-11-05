ad1d153d271ad1831dbc531568224e084785d3fa
Anissem <dima.anissem@gmail.com>
2022-11-05 18:02:11 +0300

    feat(styled-components): add styled-components to project

--- .husky/common.sh
+++ .husky/common.sh
@@ -1,7 +1,7 @@
 command_exists () {
   command -v "$1" >/dev/null 2>&1
 }
-
+#just test
 # Workaround for Windows 10, Git Bash and Yarn
 if command_exists winpty && test -t 1; then
   exec < /dev/tty
